7fee07f88e368d8143f3b7af7fd38d9954d36296
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-20 19:12:14 -0300

    update backgound fonts favcoin css

--- frontend/src/components/InputTasks.js
+++ frontend/src/components/InputTasks.js
@@ -1,41 +1,53 @@
 import React, { Fragment, useState } from "react";
 
-const InputTasks = () =>{
+const InputTasks = () => {
+  const [description, setDescription] = useState("");
 
-  const  [description, setDescription] = useState("");
-  
-  const onSubmitForm = async e => {
+  const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
-      const body = { description }
-      const response = await fetch("http://localhost:5000/tasks", {
+      const body = { description };
+      await fetch("http://localhost:5000/tasks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
       });
 
       window.location = "/";
-
     } catch (err) {
-      console.error(err.menssage)
+      console.error(err.message);
     }
-  }
+  };
 
   return (
     <Fragment>
-      <h1 className="text-center mt-5">LISTA DE TAREFAS</h1>
+      <h1
+        className="text-center mt-5"
+        style={{
+          fontFamily: "Poppins, sans-serif",
+          fontWeight: "bold",
+          fontSize: "2.4rem",
+          color: "#333",
+        }}
+      >
+        LISTA DE TAREFAS
+      </h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-        <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
-        <button className="btn btn-success mr-5">adicionar</button>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button
+          className="btn btn-success mr-5"
+          style={{ marginLeft: "10px" }}
+        >
+          adicionar
+        </button>
       </form>
-
     </Fragment>
-
-  )
-}
+  );
+};
 
 export default InputTasks;
-
-
-
-
